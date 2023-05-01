805be9566b8597b389d3b1d8b8635af1cbbe5c92
pranavnair201 <pranav.nair@sculptsoft.com>
2023-05-01 18:11:22 +0530

    db config

--- backend/script.sh
+++ backend/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip3 install -r requirements.txt
-# flask db init
-# flask db migrate -m "initial migration"
+flask db init
+flask db migrate -m "initial migration"
 flask db upgrade
 python app.py
