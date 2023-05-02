2a35ffc61b2288f55a459cc41323d1645a301f7c
pranavnair201 <pranav.nair@sculptsoft.com>
2023-05-02 11:17:01 +0530

    twilio config 2

--- backend/utils/twilio.py
+++ backend/utils/twilio.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 # from authy.api import AuthyApiClient
 
 # phone_auth = AuthyApiClient(os.environ.get('AUTHY_API_KEY'))
-account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
-auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+account_sid = "AC0a719bd300142a191cbb04e544a40266"#os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = "da2e2e50d2f8cd3f622c3bc6647409eb"#os.environ.get('TWILIO_AUTH_TOKEN')
 phone_auth = Client(account_sid, auth_token)
 
 service = phone_auth.verify.v2.services.create(friendly_name='My Verify Service')
